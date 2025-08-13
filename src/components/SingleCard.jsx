6db8aacd568e4e754f36d0390a87d743b364a7b8
Nancy Quinonez <nancyq29@hotmail.com>
2025-08-13 18:34:15 -0500

    Cont. logic on single card comp.

--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,5 +1,18 @@
-function SingleCard(){
+import { useEffect, useState } from "react"
 
+
+function SingleCard(){
+    const [oneCard, setOneCard] = useState([])
+
+    useEffect(()=>{
+        const getOneCard = async() => {
+            try{
+
+            } catch(err) {
+                console.error(err);
+            }
+        }
+    }, [])
 
     return(
         <>
