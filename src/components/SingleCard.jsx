5239f5ad03396a47db620559be19644db0b26cf0
Nancy Quinonez <nancyq29@hotmail.com>
2025-08-16 11:00:47 -0500

    Working on single card comp.

--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react"
+import { Link, useParams } from "react-router-dom"
 
 
 function SingleCard(){
-    const [oneCard, setOneCard] = useState([])
+    const [oneCard, setOneCard] = useState([]);
+
+    const { id } = useParams();
 
     useEffect(()=>{
         const getOneCard = async() => {
@@ -15,10 +18,24 @@ function SingleCard(){
             }
         }
         getOneCard();
-    }, [])
+    }, [id]);
 
     return(
         <>
+        <div>
+            <Link to="/" className="backLink">Back</Link>
+        </div>
+        <div className="singleCardContainer">
+
+            {/* <img
+            /> */}
+
+            <div>
+                <h1>{card.name}</h1>
+                <h2>Type: {type.card}</h2>
+                <h3>About: {card.desc}</h3>
+            </div>
+        </div>
         </>
     )
 }
