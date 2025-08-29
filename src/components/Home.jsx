ada9991de1cea8bf369a10401e0d71033294da73
Nancy Quinonez <nancyq29@hotmail.com>
2025-08-28 20:55:14 -0500

    Completed Home component.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router-dom"
+
 function Home(){
-    
+    const navigate = useNavigate()
+    const handleClick = () => {
+        navigate("/cards")
+    }
 
     return(
     <>
+    <h1>Take a leap of faith...</h1>
+    <button onClick={handleClick}>
+        Dive in
+    </button>
     </>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,7 @@
+
+
 function NavBar() {
+
 
     return(
         <>
