77f590f1f9973a39859211a4ec0a403bb36c2498
Nancy Quinonez <nancyq29@hotmail.com>
2025-11-11 09:51:45 -0600

    Creation of footer component, header comp., addition of logo

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,27 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Cards from './components/AllCards'
 import Home from './components/Home'
 import SingleCard from './components/SingleCard'
+import NavBar from './components/NavBar'
+import tarotLogo from './assets/tarotLogo.png'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
 
   return (
     <>
-    <h1>TarotUI</h1>
+    <h1><img id='logo' src={tarotLogo}/>TarotUI</h1>
+
+    <NavBar />
 
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/cards" element={<Cards/>}/>
       <Route path="/cards/:cardId" element={<SingleCard/>}/>
     </Routes>
+
+    <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+
+    return(
+        <>
+            <p className="footer">©2025 Nancy Quinonez. Created with heart and soul. All rights reserved.</p>
+        </>
+    )
+}
+
+export default Footer
